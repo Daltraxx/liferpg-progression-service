12fcca5d472f57f015799b1520d08d5bb8153938
Dalton Pettus <daltpettus@gmail.com>
2026-06-09 11:33:31 -0700

    include UserAggregatorService in settlement module dependencies

--- src/settlement/settlement.module.ts
+++ src/settlement/settlement.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SettlementService } from './settlement.service';
 import { TimezoneDetectorService } from './pipeline/timezone-detector.service';
+import { UserAggregatorService } from './pipeline/user-aggregator.service';
 
 @Module({
-  providers: [SettlementService, TimezoneDetectorService],
+  providers: [
+    SettlementService,
+    TimezoneDetectorService,
+    UserAggregatorService,
+  ],
 })
 export class SettlementModule {}
