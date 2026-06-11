fe57a7fc6cc9c38bcffc24f00816f4f26fda9edb
Dalton Pettus <daltpettus@gmail.com>
2026-06-11 14:04:56 -0700

    add userprocessorservice to module

--- src/settlement/settlement.module.ts
+++ src/settlement/settlement.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { SettlementService } from './settlement.service';
 import { TimezoneDetectorService } from './pipeline/timezone-detector.service';
 import { UserAggregatorService } from './pipeline/user-aggregator.service';
+import { UserProcessorService } from './pipeline/user-processor.service';
 
 @Module({
   providers: [
     SettlementService,
     TimezoneDetectorService,
     UserAggregatorService,
+    UserProcessorService,
   ],
 })
 export class SettlementModule {}
