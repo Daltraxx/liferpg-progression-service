fbdcbfadc7de87b67b88fcd192f0bcde68d148f6
Dalton Pettus <daltpettus@gmail.com>
2026-06-13 13:46:06 -0700

    switch to createApplicationContext since we dont need an http server

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  // Application context is created, but we don't need to start an HTTP server since this is a background service
 }
 bootstrap();
