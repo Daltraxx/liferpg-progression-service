35f17c64f712335c6edf837c9fd451f5b9d33a90
Dalton Pettus <daltpettus@gmail.com>
2026-06-13 12:53:08 -0700

    remove unused providers and controllers from root app module and delete boilerplate files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from './database/database.module';
@@ -16,7 +14,7 @@ import { SettlementModule } from './settlement/settlement.module';
     SettlementModule,
   ],
 
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
