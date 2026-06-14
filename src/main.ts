d3e88d93fdf6b24e86c1bb82d24930ee706ccc56
Dalton Pettus <daltpettus@gmail.com>
2026-06-14 10:44:50 -0700

    add graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
-  // Application context is created, but we don't need to start an HTTP server since this is a background service
+  try {
+    const app = await NestFactory.createApplicationContext(AppModule);
+    // Application context is created, but we don't need to start an HTTP server since this is a background service
+
+    // Handle graceful shutdown
+    const shutdownSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    shutdownSignals.forEach((signal) => {
+      process.on(signal, async () => {
+        console.log(`Received ${signal}, shutting down gracefully...`);
+        await app.close();
+        process.exit(0);
+      });
+    });
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
